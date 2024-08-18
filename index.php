140d677805f40dff5d4b1beb849a6fef0dc50af9
Maxim <maxthesheik@gmail.com>
2024-08-18 19:03:31 +0200

    xxxxx

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-// phpinfo();
-
-=======
 
 // phpinfo();
->>>>>>> d458382 (rimosso phpinfo)
-=======
 
-// phpinfo();
->>>>>>> d458382362285e12c2810e1926c0e31cef3215a5
