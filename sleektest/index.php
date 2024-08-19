432d5c57c15f8696d13533f8bffbb78e2ddd8e96
Gargantua <maxthesheik@gmail.com>
2024-08-19 12:21:30 +0200

    Create post and users

--- sleektest/index.php
+++ sleektest/index.php
@@ -2,13 +2,15 @@
 
 
 // var_dump(__DIR__);
-
+use SleekDB\Store;
 
 include __DIR__ . "/../SleekDBsrc/Store.php";
 
 $databaseDirectory = __DIR__ . "/../SleekDatabase";
 
 $newsStore = new \SleekDB\Store("news", $databaseDirectory);
+$userStore = new Store('users', $databaseDirectory);
+$postStore = new Store('posts', $databaseDirectory);
 
 $article = [
     "title" => "Google Pixel XL",
@@ -20,9 +22,40 @@ $article = [
    ];
 $results = $newsStore->insert($article);
 
+$userStore->insert([
+    'name' => 'Mike Doe',
+    'email' => 'miked@example.com',
+    'avatar' => [
+      'sm' => "/img-sm.jpg",
+      'lg' => "/img-lg.jpg"
+    ]
+  ]);
+
+
+
+
+
+
 $allNews = $newsStore->findAll();
 
+
+$userStore->insert([
+    'name' => 'Mike Doe',
+    'email' => 'miked@example.com',
+    'avatar' => [
+      'sm' => "/img-sm.jpg",
+      'lg' => "/img-lg.jpg"
+    ]
+  ]);
+
+
+$allUsers = $userStore->findAll();
+
+
+
+
 print_r($allNews);
+print_r($allUsers);
 
 
 // echo "<H1>Sleek Test</H1>";
